73b65faa1ce51de88991e3c5d0b65c4502da2326
Liang Wang <ryanrhymes@gmail.com>
2012-05-29 13:59:40 +0300

    add sigcomm-icon.png

--- include/header.php
+++ include/header.php
@@ -75,6 +75,14 @@
 						}
 					}
 				);
+				
+				//Google
+				(function() {
+    				var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+    				po.src = 'https://apis.google.com/js/plusone.js';
+    				var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+  				})();
+  				
 		    }
 		    catch(err)
 		    {
@@ -83,14 +91,6 @@
 
 		});
   	</script>
-  	
-  	<script type="text/javascript">
-  	(function() {
-    	var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-    	po.src = 'https://apis.google.com/js/plusone.js';
-    	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-  	})();
-	</script>
 
 </head>
 
