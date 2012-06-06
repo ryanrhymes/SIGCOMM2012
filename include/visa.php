b33811a2c8b11dab3a5dfd22d29c32f892965990
Lars Eggert <lars@eggert.org>
2012-06-06 09:37:56 +0200

    Update visa support letter requirements based on the ACM's official information.

--- include/visa.php
+++ include/visa.php
@@ -3,9 +3,10 @@
 Citizens of some countries are required to have a <b>visa</b> for
 entry into Finland. Please check if you require a visa from the <a
 href="http://formin.finland.fi/public/default.aspx?nodeid=15720&amp;contentlan=2&amp;culture=en-US"
-rel="external">Ministry for
-				 Foreign Affairs</a>. 
-</p><p>				 
+rel="external">Ministry for Foreign Affairs</a>. 
+</p>
+
+<p>				 
 				 
 				 As part of the
 				 visa application process, you may be
@@ -16,13 +17,15 @@ rel="external">Ministry for
 				 href="mailto:supportletters@acm.org">supportletters@acm.org</a>,
 				 and include
 <ul>
-<li> Your name</li>
-<li> Your mailing address</li>
-<li> Your fax number</li>
-<li> The name of the conference you are attending (ACM SIGCOMM 2012) and
-				 your registration ID </li>
-<li> If you are an author of an accepted paper or poster, please
-				 provide the title</li>
+<li> Your name as it appears on your passport</li>
+<li> Your current postal mailing address</li>
+<li> The name of the conference you wish to attend ("ACM SIGCOMM 2012")</li>
+<li> Your Registration Confirmation Number</li>
+<li> If you have any papers accepted for the conference, please provide the title and indicate whether you are the "sole author" or a "co- author"</li>
 </ul>
+
+<p>
+Visa support letters are answered in the order they are received. You will receive a PDF letter by email within the next 4-5 business days.  Please note we do not fax visa support letters.  The PDF serves as a signed electronic version of the original letter which will be mailed to your current mailing address.  If you do not receive the letter by post mail on time for your visa appointment, please print out your PDF letter and take it with you.
+</p>
 
 <a href="mailto:supportletters@acm.org" data-role="button" class="button">Request Visa Support Letter</a>
