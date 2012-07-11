a19018cde568cbaddd964dc6a20ccae72f81cb6a
Lars Eggert <lars@eggert.org>
2012-07-11 18:37:03 +0200

    Minor formatting change to copyright.

--- include/footer.php
+++ include/footer.php
@@ -2,8 +2,9 @@
 		<?php include ("menu.php"); ?>
 	</div><!-- /content -->
 
-<div id="license-div">
-<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-nc-sa/3.0/88x31.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">ACM SIGCOMM 2012 Website</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="http://conferences.sigcomm.org/sigcomm/2012/" property="cc:attributionName" rel="cc:attributionURL">ACM SIGCOMM 2012</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/">Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License</a>. Source code is available at <a href="https://github.com/ryanrhymes/SIGCOMM2012">github</a>.
+<div class="license">
+<!--<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-nc-sa/3.0/88x31.png" /></a>-->
+The <span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">ACM SIGCOMM 2012 Website</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="http://conferences.sigcomm.org/sigcomm/2012/" property="cc:attributionName" rel="cc:attributionURL">ACM SIGCOMM 2012</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/">Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License</a>. Source code is available at <a href="https://github.com/ryanrhymes/SIGCOMM2012">github.com</a>.
 </div>
 
 <?php if ($mypage != "supporters.php")
