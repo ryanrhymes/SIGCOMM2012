6ba1ab322c185cc72260e4e09aed5c8411df9a7d
Liang Wang <ryanrhymes@gmail.com>
2012-11-19 14:07:02 +0200

    minor changes

--- program.php
+++ program.php
@@ -649,7 +649,7 @@ Multi-dimensional Packet Classification",
 			"Hongqiang Harry Liu (Yale University), Ye Wang (Yale University), Yang Richard Yang (Yale University), Hao Wang (Google), Chen Tian (Yale University)",
 			"Paper",
 			"slides/session8/03new-cmo-sigcomm2012-v12.pdf",
-			"video/SIGCOMM-VIII-03-OptimizingCostAndPerformanceForContentMultihoming");
+			"video/SIGCOMM-VIII-03-OptimizingCostAndPerformanceForContentMultihoming.mp4");
 		?>
 
 		<?php tprog_add_session("12:30-14:00", "Lunch Break", ""); ?>
